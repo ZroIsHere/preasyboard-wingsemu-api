3dcee9e8ed83db0b2befe501f69e0aa21c44f623
ZroFreaks <42916282+ZroIsHere@users.noreply.github.com>
2022-10-08 22:59:03 +0200

    Completly account service

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -21,12 +21,66 @@ public class AccountController : Controller
         _container = container;
     }
 
-    [HttpGet("GetAccountByName")]
-    public AccountDTO GetAccountByName(string accountname)
+    [HttpGet("LoadAccountByName")]
+    public AccountDTO LoadAccountByName(string accountname)
     {
         return _container.GetService<IAccountService>().LoadAccountByName(new()
         {
             Name = accountname
         }).Result.AccountDto;
+    }
+    
+    [HttpGet("LoadAccountById")]
+    public AccountDTO LoadAccountById(long id)
+    {
+        return _container.GetService<IAccountService>().LoadAccountById(new()
+        {
+            AccountId = id
+        }).Result.AccountDto;
+    }
+
+    [HttpPost("SaveAccount")]
+    public AccountSaveResponse SaveAccount(AccountDTO dto)
+    {
+        return _container.GetService<IAccountService>().SaveAccount(new()
+        {
+            AccountDto = dto
+        }).Result;
+    }
+
+    [HttpPost("GetAccountBan")]
+    public AccountBanGetResponse GetAccountBan(long id)
+    {
+        return _container.GetService<IAccountService>().GetAccountBan(new()
+        {
+            AccountId = id
+        }).Result;
+    }
+
+    [HttpPost("SaveAccountBan")]
+    public AccountBanSaveResponse SaveAccountBan(AccountBanDto dto)
+    {
+        return _container.GetService<IAccountService>().SaveAccountBan(new()
+        {
+            AccountBanDto = dto
+        }).Result;
+    }
+
+    [HttpPost("GetAccountPenalties")]
+    public AccountPenaltyGetAllResponse GetAccountPenalties(long id)
+    {
+        return _container.GetService<IAccountService>().GetAccountPenalties(new()
+        {
+            AccountId = id
+        }).Result;
+    }
+
+    [HttpPost("SaveAccountPenalties")]
+    public AccountPenaltyMultiSaveResponse SaveAccountPenalties(List<AccountPenaltyDto> list)
+    {
+        return _container.GetService<IAccountService>().SaveAccountPenalties(new()
+        {
+            AccountPenaltyDtos = list
+        }).Result;
     }
 }
