46caa5c4251be3411fd23b28a4fb7f0d28407613
ZroFreaks <42916282+ZroIsHere@users.noreply.github.com>
2022-10-11 10:30:29 +0200

    Update NosWebAppEnvVariables.cs

--- NosWebAppEnvVariables.cs
+++ NosWebAppEnvVariables.cs
@@ -5,7 +5,7 @@ namespace noswebapp_api;
 public class NosWebAppEnvVariables
 {
     static string _AuthKey = "";
-    static string _EncryptionKey = -1;
+    static string _EncryptionKey = "";
     public static string AuthKey
     {
         get
@@ -22,7 +22,7 @@ public class NosWebAppEnvVariables
     {
         get
         {
-            if (_EncryptionKey == -1)
+            if (string.IsNullOrEmpty(_EncryptionKey))
             {
                 _EncryptionKey = Environment.GetEnvironmentVariable("API_ENCRYPTION_KEY") ?? "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAp/DpfsTzvSYYEPBnxraGt8VdQBcF53883wYR3WrT/Jr0vhhgBCFF7DMLOgbjZ0gbH6rCVI02AaPECapvJ7E5KkgZIrooGEiKf/K0r/rRqmgl9gTN8Nnl5ePRYA/J26SbqrpqgIaUUsnEEtiyu+2G0lAEuA61h2Q//WcqoVX44rYHCYTK/I4DL+ACNbkgklmX67LYApbGZW6wf4Q9Cq/XfyusCx0MbZjLepcJAACCOenkXFPu0zOBZ6r/XsNypR18Gg9DhZ/kbqrZWEi9ClyrGMe/zkXJmCDbcJ+C4rI6Gn7zwOLKSVV51DMgh8QG38WD4ga5XbBdHSg2nSsxnQR+mwIDAQAB";
             }
