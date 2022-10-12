1f09f0bd33880e2c9165905a45336124fb976136
ZroFreaks <42916282+ZroIsHere@users.noreply.github.com>
2022-10-12 23:51:49 +0200

    Added decrypt string extension

---
+++ Extensions/StringExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Security;
+using ProtoBuf.Serializers;
+
+namespace noswebapp_api.Extensions;
+
+public static class StringExtension
+{
+    //Is inversed, the key is the public, but i will put private in all because is the real system.
+    public static string DecryptWithPrivateKey(this string value)
+    {
+        using TextReader privateKeyTextReader = new StringReader(File.ReadAllText("public_key.perm"));
+        AsymmetricCipherKeyPair readKeyPair = (AsymmetricCipherKeyPair)new PemReader(privateKeyTextReader).ReadObject();
+
+        RSAParameters rsaParams = DotNetUtilities.ToRSAParameters((RsaPrivateCrtKeyParameters)readKeyPair.Private);
+        RSACryptoServiceProvider csp = new RSACryptoServiceProvider();
+        csp.ImportParameters(rsaParams);
+        var bytes = csp.Decrypt(Convert.FromBase64String(value), false);
+        return Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+    }
+}
