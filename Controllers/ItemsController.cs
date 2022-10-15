5e73b4a69e910dbd6eefd66fc55eee9d9f448b1a
ZroFreaks <42916282+ZroIsHere@users.noreply.github.com>
2022-10-15 19:11:47 +0200

    no message

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -4,8 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 using noswebapp_api.Attributes;
 using noswebapp_api.RequestEntities;
 using PhoenixLib.Caching;
+using PhoenixLib.MultiLanguage;
 using WingsAPI.Data.GameData;
 using WingsEmu.DTOs.Items;
+using WingsEmu.Game._i18n;
 
 namespace noswebapp_api.Controllers;
 
@@ -13,19 +15,25 @@ namespace noswebapp_api.Controllers;
 [Route("[controller]")]
 public class ItemsController : Controller
 {
-    private readonly IReadOnlyList<ItemDTO> _cachedItems;
+    private readonly List<ItemDTO> _cachedItems;
+    private readonly List<GameDataTranslationDto> _cachedTexts;
     private readonly IResourceLoader<ItemDTO> _itemDao;
 
-    public ItemsController(IResourceLoader<ItemDTO> itemDao, ILongKeyCachedRepository<ItemDTO> cachedItems)
+    public ItemsController(IResourceLoader<ItemDTO> itemDao, IResourceLoader<GameDataTranslationDto> textsDao)
     {
-        _cachedItems = itemDao.LoadAsync().Result;
+        _cachedTexts = textsDao.LoadAsync().Result.Where(s => s.Language.Equals(RegionLanguageType.EN) && s.DataType.Equals(GameDataType.Item)).ToList();
+        foreach (ItemDTO itemDto in itemDao.LoadAsync().Result)
+        {
+            itemDto.Name = _cachedTexts.FirstOrDefault(s => s.Key.Equals(itemDto.Name)).Value;
+            _cachedItems.Add(itemDto);
+        }
     }
     
     [Authorize]
     [HttpGet("GetAll")]
     public List<ItemDTO> GetAll()
     {
-        return _cachedItems.ToList();
+        return _cachedItems;
     }
     
     [Authorize]
