47c4635039bb972b2d15a2d8d2845750895837ff
ZroFreaks <42916282+ZroIsHere@users.noreply.github.com>
2022-11-24 14:21:28 +0100

    Add timespace controller

---
+++ Controllers/TimeSpaceController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using PreasyBoard.Api.Attributes;
+using PreasyBoard.Api.RequestEntities;
+using System;
+using WingsAPI.Communication.DbServer.TimeSpaceService;
+
+namespace PreasyBoard.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TimeSpaceController : Controller
+    {
+        private readonly IServiceProvider _container;
+
+        private readonly ILogger<TimeSpaceController> _logger;
+
+        public TimeSpaceController(ILogger<TimeSpaceController> logger, IServiceProvider container)
+        {
+            _logger = logger;
+            _container = container;
+        }
+
+        [Authorize]
+        [HttpPost("GetTimeSpaceRecord")]
+        public TimeSpaceRecordResponse GetTimeSpaceRecord(OnlyAnLongRequest Req)
+        {
+            return _container.GetService<ITimeSpaceService>().GetTimeSpaceRecord(new()
+            {
+                TimeSpaceId = Req.Value
+            }).Result;
+        }
+    }
+}
