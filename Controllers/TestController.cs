f61e89a89d2112bd9a92e09be7443e175fff1564
ZroFreaks <42916282+ZroIsHere@users.noreply.github.com>
2022-11-25 12:13:43 +0100

    Test api controller

---
+++ Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PreasyBoard.Api.Attributes;
+using System;
+
+namespace PreasyBoard.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TestController : Controller
+{
+    private readonly IServiceProvider _container;
+
+    private readonly ILogger<TestController> _logger;
+
+    public TestController(ILogger<TestController> logger, IServiceProvider container)
+    {
+        _logger = logger;
+        _container = container;
+    }
+
+    [Authorize]
+    [HttpGet("TestApi")]
+    public ActionResult TestApi() => Ok("Working");
+}
