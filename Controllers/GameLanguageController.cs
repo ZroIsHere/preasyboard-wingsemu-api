f8ec2af2a2a12770aafc24d1d7e4b1b1a00e53ec
ZroFreaks <42916282+ZroIsHere@users.noreply.github.com>
2022-11-27 17:17:06 +0100

    Add GameLanguageService.cs

---
+++ Controllers/GameLanguageController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using PreasyBoard.Api.Attributes;
+using System;
+using WingsAPI.Communication;
+using WingsAPI.Communication.Families;
+using WingsAPI.Communication.Translations;
+using WingsEmu.Game._i18n;
+
+namespace PreasyBoard.Api.Controllers;
+
+public class GameLanguageController : Controller
+{
+    private readonly IServiceProvider _container;
+
+    private readonly ILogger<GameLanguageController> _logger;
+    public GameLanguageController(ILogger<GameLanguageController> logger, IServiceProvider container)
+    {
+        _logger = logger;
+        _container = container;
+    }
+
+    [Authorize]
+    [HttpGet("GetTranslations")]
+    public GetTranslationsResponse GetTranslations(EmptyRpcRequest Req) =>
+        _container.GetService<ITranslationService>().GetTranslations(Req).Result;
+
+    [Authorize]
+    [HttpGet("GetForbiddenWords")]
+    public GetForbiddenWordsResponse GetForbiddenWords(EmptyRpcRequest Req) =>
+        _container.GetService<ITranslationService>().GetForbiddenWords(Req).Result;
+}
